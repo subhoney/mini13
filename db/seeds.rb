2b3acb8b521748010ebe7f71b3e37235378fdd30
Joshua Broughton <jbroughton@ggc.edu>
2016-05-07 23:31:11 +0000

    9.5

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
-99.times do |n| # 99 problems, but a user ain't one
+99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
