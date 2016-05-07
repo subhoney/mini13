008085ed045c14dbeb6c0ab9fcc25d24e3e2e04a
Joshua Broughton <jbroughton@ggc.edu>
2016-05-07 00:35:41 +0000

    Fig 8.3

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
   resources :users
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 end
